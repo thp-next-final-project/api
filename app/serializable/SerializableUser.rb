ea5930a3c8808304031a803d18659d06f7ade37d
benedictebcl <bouclonbenedicte@gmail.com>
2021-03-24 22:02:09 +0100

    update

--- app/serializable/SerializableUser.rb
+++ app/serializable/SerializableUser.rb
@@ -1,5 +1,5 @@
 class SerializableUser < JSONAPI::Serializable::Resource
   type 'users'
 
-  attributes :email, :firstname, :lastname, :role
+  attributes :email, :firstname, :lastname, :role, :age, :weight, :height, :activity, :sexe, :objectif
 end
